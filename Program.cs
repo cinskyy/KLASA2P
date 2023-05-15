0917fc146116e1319488bcd58e705d3edf08f652
Your New Name <Your new email>
2023-05-15 12:12:36 +0200

    wersja 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsola2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Klasa 2P")
+            Console.WriteLine("grupa2")
+            Console.ReadLine();
+
+        }
+    }
+}
